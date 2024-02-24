ff54e6d0b6ae73b780772f0bbba61112ae23ccad
niclinn <linn23@mca.org.tw>
2024-02-24 13:39:22 -0600

    updates 2/24

--- client/src/HomePage/Components/Eventcard.js
+++ client/src/HomePage/Components/Eventcard.js
@@ -2,12 +2,11 @@ const Eventcard = () => {
     return (
         <div className="eventcard">
         <header className="Home-filter">
-            <img src = "https://via.placeholder.com" alt="Event Picture"> </img>
-            <h2></h2>
-            <h3></h3>
-            <p> </p>
-            <p> </p>
-            <p> </p>
+            <img src="https://via.placeholder.com/150" alt="Event Picture"> </img>
+            <h2>Events</h2>
+            <p>Date</p>
+            <p>Time</p>
+            <p>Location </p>
         </header>
         </div>
     );
