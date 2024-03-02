ddd9d3bf3f674e1c3607e59233c9ce6203eb05cc
EthanW67 <ew67@rice.edu>
2024-03-02 15:12:33 -0600

    Added CSS EventCard

--- client/src/HomePage/Components/Eventcard.js
+++ client/src/HomePage/Components/Eventcard.js
@@ -2,20 +2,31 @@ import React from 'react';
 import "./Eventcard.css"
 
 
-
 const Eventcard = () => {
     return (
         <div className="eventcard">
-        <header className="Home-filter">
+
+            <div>
             <img src="https://picsum.photos/200/100" alt="Event Picture" className="card-img"/>
-            <h1 className="card-title">Events</h1>
-            <p className="card-date">Date</p>
-            <p className="card-time">Time</p>
-            <p className="card-location">Location</p>
-            <a href= "cardPage" className="card-btn"> Learn More</a>
-        </header>
+            </div>
+
+            <div className="text">
+                <h1 className="card-title">Events</h1>
+                
+                <div className="card-description">
+                    <p className="card-date">Date</p>
+                    <p className="card-time">Time</p>  
+                    <p className="card-location">Location</p>                
+                </div>
+            </div>
+
+            <div className='buttons'>
+                <a href= "cardPage" className="card-btn"> Learn More</a>
+                <a href= "cardPage" className="card-btn"> View</a>
+            </div>
         </div>
+
+      
     );
 }
-
 export default Eventcard
