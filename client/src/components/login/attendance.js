9ace8048a32308ce294d6dccf1780cc2eefa07c5
niclinn <linn23@mca.org.tw>
2024-03-23 15:07:09 -0500

    sprint, worked on entry cards, need to work on backend integration:

--- client/src/components/login/attendance.js
+++ client/src/components/login/attendance.js
@@ -9,8 +9,41 @@ const Attendance = () => {
                 Viewing attendance for <strong>event name  </strong>
                 <input placeholder='name' />
             </div>
-            <div className='search-results'></div>
-            Showing 5 results for "Smith"
+            <div className='search-results'>Showing 5 results for "Smith"</div>
+
+            <div className="entry-header">Name Ticket Type Payment Status Seat Number</div>
+            <div className="user-cards">
+                <div className="card">
+                    <div className="name">My Name</div>
+                    <div className="ticket-type">Family</div>
+                    <div className="payment-status">Paid</div>
+                    <div className="seat-number">A21</div>
+                </div>
+                <div className="card">
+                    <div className="name">My Name</div>
+                    <div className="ticket-type">Family</div>
+                    <div className="payment-status">Paid</div>
+                    <div className="seat-number">A21</div>
+                </div>
+                <div className="card">
+                    <div className="name">My Name</div>
+                    <div className="ticket-type">Family</div>
+                    <div className="payment-status">Paid</div>
+                    <div className="seat-number">A21</div>
+                </div>
+                <div className="card">
+                    <div className="name">My Name</div>
+                    <div className="ticket-type">Family</div>
+                    <div className="payment-status">Paid</div>
+                    <div className="seat-number">A21</div>
+                </div>
+                <div className="card">
+                    <div className="name">My Name</div>
+                    <div className="ticket-type">Family</div>
+                    <div className="payment-status">Paid</div>
+                    <div className="seat-number">A21</div>
+                </div>
+            </div>
         </div>
     </div>
     
