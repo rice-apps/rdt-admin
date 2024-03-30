2852915b62b201c5f271731e25ecd3675e5b190e
niclinn <linn23@mca.org.tw>
2024-03-30 02:06:27 -0500

    styled event attendance page

--- client/src/components/login/attendance.js
+++ client/src/components/login/attendance.js
@@ -1,17 +1,33 @@
 import './attendance.css'
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
 
 const Attendance = () => {
     return (
     <div className='main-div'>
         <div className='return-listings'> ← Return to Event Listings</div>
         <div className="main-content">
-            <div>
-                Viewing attendance for <strong>event name  </strong>
-                <input placeholder='name' />
+
+            <div className="search">
+                <div className="search-text">
+                    Viewing attendance for <br />
+                    <strong>Event Name</strong>
+
+                </div>
+                <div className="search-bar"><input placeholder="Search" /></div>
+                <div className="search-button">
+                    <button>Search</button>
+                </div>
             </div>
+
             <div className='search-results'>Showing 5 results for "Smith"</div>
 
-            <div className="entry-header">Name Ticket Type Payment Status Seat Number</div>
+            <div className="entry-header">
+                <div className="entry-header1">Name</div>
+                <div className="entry-header2">Ticket Type</div>
+                <div className="entry-header3">Payment Status</div>
+                <div className="entry-header4">Seat Number</div>
+            </div>
+
             <div className="user-cards">
                 <div className="card">
                     <div className="name">My Name</div>
