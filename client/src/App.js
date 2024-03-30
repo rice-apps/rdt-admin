4ab6c6541ffe507bfda16159cc10afbfe365f07a
niclinn <linn23@mca.org.tw>
2024-03-30 15:26:02 -0500

    styling update

--- client/src/App.js
+++ client/src/App.js
@@ -14,26 +14,25 @@ import Homepage from './pages/HomePage';
 function App() {
 
   return (
-
     <Attendance />
 
     // <div className="App">
     //   {
     //     <RegisterEvent></RegisterEvent>
-      
-    //   /* <Navbar name={"Quang"}/>
-    //   <header className="App-header">
-    //     <img src={"https://riceapps.org/static/media/logo_color_light.7d03c94d.png"} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    // </div>
+    //   }
+    // //   /* <Navbar name={"Quang"}/>
+    // //   <header className="App-header">
+    // //     <img src={"https://riceapps.org/static/media/logo_color_light.7d03c94d.png"} className="App-logo" alt="logo" />
+    // //     <p>
+    // //       Edit <code>src/App.js</code> and save to reload.
+    // //     </p>
+    // //     <a
+    // //       className="App-link"
+    // //       href="https://reactjs.org"
+    // //       target="_blank"
+    // //       rel="noopener noreferrer"
+    // //     >
+    // // </div>
   );
 }
 export default App;
