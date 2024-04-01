36259ce1ad50a32bb80fe8bd569fb28c55d1314e
quangnguyen <qnn1@rice.edu>
2024-03-31 23:28:32 -0500

    working backend

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,32 +1,29 @@
 import React from "react";
 import "../styles/loginpage.css";
-import testPic from '../images/testpic.png';
-import google from'../images/google.png';
+import testPic from "../images/testpic.png";
+import google from "../images/google.png";
 
 const Login = () => {
   return (
     <div className="container">
       <div className="purple-rectangle">
         <div className="text-container">
-          <img
-            className= "image"
-            src= {testPic}
-            alt = "imagetoinsert"
-          />
-</div>
-    <div className="login-container">
-      <h1 className="title">Log in to Rice Dance Theatre</h1>
-      <button className="google-signin">
-        <img
-          className="google-icon"
-          src={google}
-          alt="Google"
-        />
-        Log in with Google
-      </button>
+          <img className="image" src={testPic} alt="imagetoinsert" />
+        </div>
+        <div className="login-container">
+          <h1 className="title">Log in to Rice Dance Theatre</h1>
+          <a
+            style={{ textDecoration: "none", color: "black" }}
+            href="https://rdt-backend-develop.up.railway.app/auth/google?app=admin"
+          >
+            <button className="google-signin">
+              <img className="google-icon" src={google} alt="Google" />
+              Log in with Google
+            </button>
+          </a>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
   );
 };
 
