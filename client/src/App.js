e71439a78de629e073d2a069df4a41cc45a74833
niclinn <linn23@mca.org.tw>
2024-04-13 13:55:00 -0500

    attendance merge updates

--- client/src/App.js
+++ client/src/App.js
@@ -10,36 +10,12 @@ import RegisterEvent from './pages/RegisterEventPage';
 import EditEvent from './pages/EditEventPage';
 import Layout from './components/Layout';
 import Homepage from './pages/HomePage';
+import Login from './pages/LoginPage';
 
 function App() {
 
   return (
-
-
-    // <Attendance />
-    <Layout>
-      <LoginPage />
-      {/* <Homepage/> */}
-      {/* <RegisterEvent/>  */}
-      {/* <EditEvent/> */} 
-
-    // <div className="App">
-    //   {
-    //     <RegisterEvent></RegisterEvent>
-    //   }
-    // //   /* <Navbar name={"Quang"}/>
-    // //   <header className="App-header">
-    // //     <img src={"https://riceapps.org/static/media/logo_color_light.7d03c94d.png"} className="App-logo" alt="logo" />
-    // //     <p>
-    // //       Edit <code>src/App.js</code> and save to reload.
-    // //     </p>
-    // //     <a
-    // //       className="App-link"
-    // //       href="https://reactjs.org"
-    // //       target="_blank"
-    // //       rel="noopener noreferrer"
-    // //     >
-    // // </div>
+    null
   );
 }
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -29,7 +29,7 @@ const router = createBrowserRouter([
     element: <EditEvent />,
   },
   {
-    path: "/",
+    path: "/attendance",
     element: <Attendance />,
   },
 ]);
